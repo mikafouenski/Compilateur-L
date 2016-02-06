1951ab2d0babd479ed23f07ce745636e807e551e
Mickaël Bernardini <contact@mickael-bernardini.fr>
2016-02-06 17:03:02 +0100

    Add declaration of analyseur_syntaxique.*

--- analyseur_syntaxique.c
+++ analyseur_syntaxique.c
@@ -1,17 +1,20 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "symboles.h"
 #include "analyseur_syntaxique.h"
 #include "util.h"
 #include "analyseur_lexical.h"
+#include "premiers.h"
+#include "suivants.h"
 
 int uniteCourante;
 int trace_xml = 1;
 char nom[100];
 char valeur[100];
 
-void expression(void) {
+/*void expression(void) {
     affiche_balise_ouvrante("Expression", trace_xml);
     terme();
     expressionBis();
@@ -77,9 +80,133 @@ void facteur(void) {
             exit (-1);
         }
     }
+}*/
+
+
+void listeDecVariables (void) {
+
+}
+void listeDecFonctions (void) {
+
+}
+void declarationVariable (void) {
+
+}
+void declarationFonction (void) {
+
+}
+void listeParam (void) {
+
+}
+void listeInstructions (void) {
+
+}
+void instruction (void) {
+
+}
+void instructionAffect (void) {
+
+}
+void instructionBloc (void) {
+
+}
+void instructionSi (void) {
+
+}
+void instructionTantque (void) {
+
+}
+void instructionAppel (void) {
+
+}
+void instructionRetour (void) {
+
+}
+void instructionEcriture (void) {
+
+}
+void instructionVide (void) {
+
+}
+void var (void) {
+
+}
+void expression (void) {
+
+}
+void appelFct (void) {
+
+}
+void conjonction (void) {
+
+}
+void negation (void) {
+
+}
+void comparaison (void) {
+
+}
+void expArith (void) {
+
+}
+void terme (void) {
+
+}
+void facteur (void) {
+
+}
+void argumentsEffectifs (void) {
+
+}
+void listeExpressions (void) {
+
+}
+void listeExpressionsBis (void) {
+
+}
+void programme (void) {
+
+}
+void conjonctionBis (void) {
+
+}
+void optTailleTableau (void) {
+
+}
+void expArithBis (void) {
+
+}
+void optSinon (void) {
+
+}
+void comparaisonBis (void) {
+
+}
+void optDecVariables (void) {
+
+}
+void optIndice (void) {
+
+}
+void listeDecVariablesBis (void) {
+
+}
+void termeBis (void) {
+
+}
+void expressionBis (void) {
+
+}
+void instructionFaire (void) {
+
+}
+void optListeDecVariables (void) {
+
 }
 
 void syntaxe(void) {
+    initialise_premiers();
+    initialise_suivants();
     uniteCourante = yylex();
     expression();
 }
--- analyseur_syntaxique.h
+++ analyseur_syntaxique.h
@@ -1,11 +1,47 @@
 #ifndef __SYNTAX__
 #define __SYNTAX__
 
-void expression(void);
-void expressionBis(void);
-void terme(void);
-void termeBis(void);
-void facteur(void);
+void listeDecVariables (void);
+void listeDecFonctions (void);
+void declarationVariable (void);
+void declarationFonction (void);
+void listeParam (void);
+void listeInstructions (void);
+void instruction (void);
+void instructionAffect (void);
+void instructionBloc (void);
+void instructionSi (void);
+void instructionTantque (void);
+void instructionAppel (void);
+void instructionRetour (void);
+void instructionEcriture (void);
+void instructionVide (void);
+void var (void);
+void expression (void);
+void appelFct (void);
+void conjonction (void);
+void negation (void);
+void comparaison (void);
+void expArith (void);
+void terme (void);
+void facteur (void);
+void argumentsEffectifs (void);
+void listeExpressions (void);
+void listeExpressionsBis (void);
+void programme (void);
+void conjonctionBis (void);
+void optTailleTableau (void);
+void expArithBis (void);
+void optSinon (void);
+void comparaisonBis (void);
+void optDecVariables (void);
+void optIndice (void);
+void listeDecVariablesBis (void);
+void termeBis (void);
+void expressionBis (void);
+void instructionFaire (void);
+void optListeDecVariables (void);
+
 void syntaxe(void);
 
 #endif
