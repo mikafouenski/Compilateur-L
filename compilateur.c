232d1c604d5557b0493284c9c696e1bd23d99b37
Mickaël Bernardini <contact@mickael-bernardini.fr>
2016-02-09 17:22:57 +0100

    Add mulit params -s and -l

--- compilateur.c
+++ compilateur.c
@@ -7,13 +7,44 @@
 char yytext[100];
 FILE *yyin;
 
+int lflg, sflg;
+int opterr = 0;
+
 int main(int argc, char **argv) {
-  yyin = fopen(argv[1], "r");
-  if(yyin == NULL){
-    fprintf(stderr, "impossible d'ouvrir le fichier %s\n", argv[1]);
-    exit(1);
-  }
-  //test_yylex_internal(yyin);
-  syntaxe();
+
+    int c;
+    if (argc - 1 != 2) {
+        printf("Met -l ou -s avant le fichier !\n");
+        exit(1);
+    }
+    while ((c=getopt(argc, argv, "sl")) != EOF) switch(c) {
+        case 's':
+            sflg++;
+            continue;
+
+        case 'l':
+            lflg++;
+            continue;
+
+        case '?':
+            opterr++;
+            continue;
+        default:
+            opterr++;
+            break;
+    }
+    if(opterr) {
+        fprintf(stderr,"usage: compilateur -sl file ...\n");
+        exit(1);
+    }
+    yyin = fopen(argv[2], "r");
+    if(yyin == NULL){
+        fprintf(stderr, "impossible d'ouvrir le fichier %s\n", argv[2]);
+        exit(1);
+    }
+    if (lflg)
+        test_yylex_internal(yyin);
+    if (sflg)
+        syntaxe();
   return 0;
 }
