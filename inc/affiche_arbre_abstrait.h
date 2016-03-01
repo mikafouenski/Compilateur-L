3f84899c59c290bf54ef616d0beaec2bd8c89d01
Mickaël Bernardini <contact@mickael-bernardini.fr>
2016-03-01 13:56:50 +0100

    Add files from ametice

---
+++ inc/affiche_arbre_abstrait.h
@@ -0,0 +1,163 @@
+
+
+<!DOCTYPE html>
+
+
+
+
+
+
+
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+  
+ <!-- <title>CAS &#8211; Central Authentication Service</title>-->
+    <title>Aix-Marseille Universit&eacute; - Authentification</title>
+
+             
+				
+  
+  
+  <link rel="stylesheet" href="/cas/css/cas.css;jsessionid=2CA938121AA3A996172ECBCCA71D3FE2" />
+  <link rel="icon" href="/cas/favicon2.ico;jsessionid=2CA938121AA3A996172ECBCCA71D3FE2" type="image/x-icon" />
+  
+  <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.1/html5shiv.js" type="text/javascript"></script>
+  <![endif]-->
+</head>
+<body id="cas">
+  <div id="centre">
+      <header>
+         <div id="header" class="flc-screenNavigator-navbar fl-navbar fl-table">
+                <h1 id="app-name" class="fl-table-cell">Aix-Marseille Universit&eacute; - Service d'authentification</h1>
+         </div>	
+         
+        <!-- <a id="logo" href="http://www.jasig.org" title="allez à la page d'accueil Jasig">Jasig</a>
+        <h1>Central Authentication Service (CAS)</h1> -->
+      </header>
+      <div id="content">
+
+
+
+
+<div class="box" id="login">
+  <form id="fm1" action="/cas/login;jsessionid=2CA938121AA3A996172ECBCCA71D3FE2?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php" method="post">
+
+    
+  
+    <h2>Entrez votre identifiant et votre mot de passe.</h2>
+  
+    <section class="row">
+      <label for="username"><span class="accesskey">I</span>dentifiant:</label>
+      
+        
+        
+          
+          <input id="username" name="username" class="required" tabindex="1" accesskey="i" type="text" value="" size="25" autocomplete="off"/>
+        
+      
+    </section>
+    
+    <section class="row">
+      <label for="password"><span class="accesskey">M</span>ot de passe:</label>
+      
+      
+      <input id="password" name="password" class="required" tabindex="2" accesskey="m" type="password" value="" size="25" autocomplete="off"/>
+    </section>
+    
+    <section class="row check">
+      <input id="warn" name="warn" value="true" tabindex="3" accesskey="p" type="checkbox" />
+      <label for="warn"><span class="accesskey">P</span>révenez-moi avant d'accéder à d'autres services.</label>
+    </section>
+    
+    <section class="row btn-row">
+      <input type="hidden" name="lt" value="LT-2802456-EzxdZSnfLPQe3ya5qr5mZh5KNjJDOt-ident.univ-amu.fr" />
+      <input type="hidden" name="execution" value="e1s1" />
+      <input type="hidden" name="_eventId" value="submit" />
+
+      <input class="btn-submit" name="submit" accesskey="l" value="SE CONNECTER" tabindex="4" type="submit" />
+      <input class="btn-reset" name="reset" accesskey="c" value="EFFACER" tabindex="5" type="reset" />
+    </section>
+  </form>
+</div>
+  
+<div id="sidebar">
+  <div class="sidebar-content">
+    <p>Pour des raisons de sécurité, veuillez vous déconnecter et fermer votre navigateur lorsque vous avez fini d'accéder aux services authentifiés.</p>
+    
+    <div id="list-languages">
+      
+      
+      
+      
+      <h3>Languages:</h3>
+      
+      
+        
+        
+          
+          <ul>
+            <li class="first"><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=en">English</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=es">Spanish</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=fr">French</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=ru">Russian</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=nl">Nederlands</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=sv">Svenska</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=it">Italiano</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=ur">Urdu</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=zh_CN">Chinese (Simplified)</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=zh_TW">Chinese (Traditional)</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=de">Deutsch</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=ja">Japanese</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=hr">Croatian</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=cs">Czech</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=sl">Slovenian</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=ca">Catalan</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=mk">Macedonian</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=fa">Farsi</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=ar">Arabic</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=pt_PT">Portuguese</a></li>
+            <li><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=pt_BR">Portuguese (Brazil)</a></li>
+            <li class="last"><a href="login?service=https%3A%2F%2Fametice.univ-amu.fr%2Flogin%2Findex.php&locale=pl">Polish</a></li>
+          </ul>
+        
+      
+    </div>
+  </div>
+</div>
+
+<div id="univmed">
+				<p><B>Probl&egrave;me de connexion : </B>  <A HREF=http://dosi.univ-amu.fr/public_content/contacts>Aide</A> .<br>
+			Pensez &agrave; <A HREF=https://sesame.univ-amu.fr/Activation.php>activer votre compte</A> lors de votre premi&egrave;re connexion.</p>
+</div>
+
+
+
+
+      </div> <!-- END #content -->
+      
+      <footer>
+        <!--<div id="copyright">
+          <p>Copyright &copy; 2005&ndash;2012 Jasig, Inc. Tous droits réservés.</p>
+          <p>Powered by <a href="http://www.jasig.org/cas">Jasig Central Authentication Service 4.0.6</a></p>
+        </div> -->
+        <div id="footer"></div>
+      </footer>
+
+    </div> <!-- END #container -->
+    
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+    
+    
+    <script type="text/javascript" src="https://github.com/cowboy/javascript-debug/raw/master/ba-debug.min.js"></script>
+    
+    
+    <script type="text/javascript" src="/cas/js/cas.js;jsessionid=2CA938121AA3A996172ECBCCA71D3FE2"></script>
+  </body>
+</html>
+
+
